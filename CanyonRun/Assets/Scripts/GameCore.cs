b396ca6302ed77472264d327347427596565df18
Jordon Dodds <Jordon.Dodds@hotmail.com.au>
2015-10-03 00:00:18 +1000

    "Lerp" function added with comments

--- CanyonRun/Assets/Scripts/GameCore.cs
+++ CanyonRun/Assets/Scripts/GameCore.cs
@@ -2,6 +2,13 @@
 
 public class GameCore 
 {
+    // from value, to value and percentage between them
+    public float Lerp ( float MinV, float MaxV, float Progress)      // From value (MinV), To value (MaxV) and percentage Difference value (Progress)
+    {
+        return Progress * (MaxV - MinV);                             // Progress (0-1) x (Maximum value - Minimum value), where 1 = 100%
+    }
+
+
     public int Level { get; set; }                      // Player level, must be at least 1
     public int XP { get; set; }                         // Total experience
     public string BoostActive { get; set; }             // Shows if experience boost is active
@@ -9,4 +16,5 @@ public class GameCore
     public string FireOnCooldown { get; set; }          // Shows if weapons are on cooldown
     public float FireCooldownRemaining { get; set; }    // Shows time remaing until weapon can fire again
     public string CanFire { get; set; }                 // Shows if weapons can currently be fired
+
 }
