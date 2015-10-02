4ce6be09cc52d3f168137d8b2e08bad1a2fcf090
Jordon Dodds <Jordon.Dodds@hotmail.com.au>
2015-10-02 23:33:57 +1000

    Added the properties along with comments explaining what they are

--- CanyonRun/Assets/Scripts/GameCore.cs
+++ CanyonRun/Assets/Scripts/GameCore.cs
@@ -1,5 +1,12 @@
 ﻿using UnityEngine;
 
-public class GameCore {
-	// Add your code here
+public class GameCore 
+{
+    public int Level { get; set; }                      // Player level, must be at least 1
+    public int XP { get; set; }                         // Total experience
+    public string BoostActive { get; set; }             // Shows if experience boost is active
+    public float BoostTimeRemaining { get; set; }       // Shows time remaining for boost
+    public string FireOnCooldown { get; set; }          // Shows if weapons are on cooldown
+    public float FireCooldownRemaining { get; set; }    // Shows time remaing until weapon can fire again
+    public string CanFire { get; set; }                 // Shows if weapons can currently be fired
 }
