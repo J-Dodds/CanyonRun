cee3be13e564c25bd4e209098f818d948648d140
Jordon Dodds <Jordon.Dodds@hotmail.com.au>
2015-10-07 16:43:42 +1000

    Realized properties are not implemented properly Implemented "Level" property properly - Dependant on "XP" property Commented

--- CanyonRun/Assets/Scripts/GameCore.cs
+++ CanyonRun/Assets/Scripts/GameCore.cs
@@ -1,24 +1,54 @@
 ﻿using UnityEngine;
 
-public class GameCore 
+public class GameCore
 {
-    // from value, to value and percentage between them
-    public float Lerp ( float MinV, float MaxV, float Progress)     // From value (MinV), To value (MaxV) and percentage Difference value (Progress)
-    {
-        return Progress * (MaxV - MinV) + MinV;                     // Progress (0-1) x (Maximum value - Minimum value), where 1 = 100%
-    }
+    public int XP;
 
-    //public int Level { get; set; }                                  // Player level, must be at least 1
-    //public int XP { get; set; }                                     // Total experience
+    public int Level            // Player level
+    {
+        get
+        {
+            return (XP / 10) + 1;  // If called, will return total xp divided by 10 + 1, as the minimum level is 1
+        }
+
+        set
+        {
+            XP = (value + 1) * 10;       // Works out XP based off of the Level
+        }
+    }
+    //public int XP { get; set; }                                                     // Total XP
     //public string BoostActive { get; set; }                         // Shows if experience boost is active
     //public float BoostTimeRemaining { get; set; }                   // Shows time remaining for boost
     //public string FireOnCooldown { get; set; }                      // Shows if weapons are on cooldown
     //public float FireCooldownRemaining { get; set; }                // Shows time remaing until weapon can fire again
     //public string CanFire { get; set; }                             // Shows if weapons can currently be fired
 
-    //public int XPRequiredToNextLevel (int Level)                    // Whole number of how much xp reqired to reach the next level
-    //{
-    //    return 25 + (Level * 5);                                    // Formula for xp required. XP to level 2 is 30, level 3 = 35, level 4 = 40
-    //}
+    public float Lerp(float MinV, float MaxV, float Progress)         // From value (MinV), To value (MaxV) and percentage Difference value (Progress)
+    {
+        return Progress * (MaxV - MinV) + MinV;                        // Progress (0-1) x (Maximum value - Minimum value), where 1 = 100%
+    }
 
+
+    /* 3. XPRequiredToNextLevel
+       XPLossFromMine
+       XPGainFromMine
+       XPGainFromPickup -
+
+        4. OnFire 
+        DecreaseFireCooldownRemaining
+
+        5. Enable Boost
+        DecreaseBoostTimeRemaining
+
+
+    
+    public int AddXP(int XPChange)
+    {
+        //I. Positive value must increase
+        //II. Negative Value must decrease
+        //III. Function returns true if level changes
+
+       
+    }
+*/
 }
