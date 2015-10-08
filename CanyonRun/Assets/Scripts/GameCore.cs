c80f7dcde1e90bd9db5a0ccf9fa0068d6a5ebb35
unknown <Jdodds@BNE-28-2059-W.sae.au>
2015-10-08 17:26:14 +1100

    Properties re added, Begun implementing 'XPRequiredTONextLevel' but class ended

--- CanyonRun/Assets/Scripts/GameCore.cs
+++ CanyonRun/Assets/Scripts/GameCore.cs
@@ -2,27 +2,31 @@
 
 public class GameCore
 {
-    public int XP;
 
-    public int Level            // Player level
+    public int Level { get; set; }                                                //Current player level
+    public int XP { get; set; }                                                   // Total XP
+    public bool BoostActive { get; set; }                                         // Shows if experience boost is active
+    public float BoostTimeRemaining { get; set; }                                 // Shows time remaining for boost
+    public bool FireOnCooldown { get; set; }                                      // Shows if weapons are on cooldown
+    public float FireCooldownRemaining { get; set; }                              // Shows time remaing until weapon can fire again
+    public string CanFire { get; set; }                                           // Shows if weapons can currently be fired
+
+    public float Lerp(float MinV, float MaxV, float Progress)                     // From value (MinV), To value (MaxV) and percentage Difference value (Progress)
     {
-        get
+        return Progress * (MaxV - MinV) + MinV;                                   // Progress (0-1) x (Maximum value - Minimum value), where 1 = 100%
+    }
+
+    public int XPRequiredToNextLevel (int playerLevel)
+    {
+        if (player % 2 == 0)
         {
-            return (XP / 10) + 1;  // If called, will return total xp divided by 10 + 1, as the minimum level is 1
+            Level * 11;
+        }
+        else
+        {
+            Level * 10;
         }
     }
-    //public int XP { get; set; }                                                     // Total XP
-    //public string BoostActive { get; set; }                         // Shows if experience boost is active
-    //public float BoostTimeRemaining { get; set; }                   // Shows time remaining for boost
-    //public string FireOnCooldown { get; set; }                      // Shows if weapons are on cooldown
-    //public float FireCooldownRemaining { get; set; }                // Shows time remaing until weapon can fire again
-    //public string CanFire { get; set; }                             // Shows if weapons can currently be fired
-
-    public float Lerp(float MinV, float MaxV, float Progress)         // From value (MinV), To value (MaxV) and percentage Difference value (Progress)
-    {
-        return Progress * (MaxV - MinV) + MinV;                        // Progress (0-1) x (Maximum value - Minimum value), where 1 = 100%
-    }
-
 
     /* 3. XPRequiredToNextLevel
        XPLossFromMine
@@ -41,9 +45,7 @@ public class GameCore
     {
         //I. Positive value must increase
         //II. Negative Value must decrease
-        //III. Function returns true if level changes
-
-       
+        //III. Function returns true if level changes  
     }
 */
 }
