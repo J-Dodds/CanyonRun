9a44a7d89a3a90cb3d92905acaad328f1fca4dba
Jordon Dodds <Jordon.Dodds@hotmail.com.au>
2015-10-12 15:45:38 +1000

    XPRequiredToNextLevel, XPLossFromMine,XPGainFromMine, XPGainFromPickup roughly implemented, still needs to be balanced DecreaseFireCooldownRemaining started.

--- CanyonRun/Assets/Scripts/GameCore.cs
+++ CanyonRun/Assets/Scripts/GameCore.cs
@@ -2,48 +2,112 @@
 
 public class GameCore
 {
-    public int XP;
 
-    public int Level            // Player level
-    {
-        get
-        {
-            return (XP / 10) + 1;  // If called, will return total xp divided by 10 + 1, as the minimum level is 1
-        }
-    }
-    //public int XP { get; set; }                                                     // Total XP
-    //public string BoostActive { get; set; }                         // Shows if experience boost is active
-    //public float BoostTimeRemaining { get; set; }                   // Shows time remaining for boost
-    //public string FireOnCooldown { get; set; }                      // Shows if weapons are on cooldown
-    //public float FireCooldownRemaining { get; set; }                // Shows time remaing until weapon can fire again
-    //public string CanFire { get; set; }                             // Shows if weapons can currently be fired
+    public int Level{ get; set; }                                           // Player level
+    public int XP { get; set; }                                                     // Total XP
+    public bool BoostActive { get; set; }                         // Shows if experience boost is active
+    public float BoostTimeRemaining { get; set; }                   // Shows time remaining for boost
+    public bool FireOnCooldown { get; set; }                      // Shows if weapons are on cooldown
+    public float FireCooldownRemaining { get; set; }                // Shows time remaing until weapon can fire again
+    public bool CanFire { get; set; }                             // Shows if weapons can currently be fired
 
     public float Lerp(float MinV, float MaxV, float Progress)         // From value (MinV), To value (MaxV) and percentage Difference value (Progress)
     {
         return Progress * (MaxV - MinV) + MinV;                        // Progress (0-1) x (Maximum value - Minimum value), where 1 = 100%
     }
 
+    int XPRequiredToNextLevel(int playerLevel)    //XP to next level                  
+    {
+        if (playerLevel % 2 ==0)
+        {
+            return playerLevel * 10;            //Even levels
+        }
+        else
+        {
+            return playerLevel * 11;            //Odd levels
+        }
+    }
 
-    /* 3. XPRequiredToNextLevel
-       XPLossFromMine
-       XPGainFromMine
-       XPGainFromPickup -
+    public int XPLossFromMine ( int playerLevel )
+    {
+        return playerLevel * 2;                 //At level 1 lose 2 xp, at 5 lose 10
+    }
 
-        4. OnFire 
-        DecreaseFireCooldownRemaining
+    public int XPGainFromMine ( int playerLevel )
+    {
+        return playerLevel * 3;                 //At level 1 gain 3 xp, at 5 gain 15
+    }
+
+    public int XPGainFromPickup ( int playerLevel )
+    {
+        return playerLevel * 4;                 //At level 1 gain 4 xp, at 5 gain 20
+    }
+
+    public void OnFire ()
+    {
+        FireOnCooldown = true;
+        CanFire = false;                        //When fired, cannot fire for a time
+    }
+
+    public void DecreaseFireCooldownRemaining(int timeElapsed)
+    {
+        for (int i; i <= 3; ++i)
+        {
+            timeElapsed = i;
+            FireCooldownRemaining = timeElapsed;
+        }
+
+        FireOnCooldown = false;
+    }
+
+    /* 
 
         5. Enable Boost
         DecreaseBoostTimeRemaining
 
 
-    
+ 
     public int AddXP(int XPChange)
     {
-        //I. Positive value must increase
-        //II. Negative Value must decrease
-        //III. Function returns true if level changes
+        if (XPChange => 0)
+        {
+            if (BoostActive == true)
+            {
+                return XP + XPChange * 2;
+            }
+            else
+            {
+                return AddXP + XPChange;
+            }
+        }
+        else if (XPChange <= 0)
+        {
+            return AddXP - XPChange;
+        }
+        else
+        {
+            --XPChange;
+        }
+         
+        if (XPRequiredToNextLevel <= 0)
+        {
+            ++Level;
+        }
+        else if (XPRequiredToNextLevel => XP)
+        {
+            --Level;
+        }
 
-       
+        if (++Level || --Level)
+        {
+            return true;
+        } 
     }
-*/
+    
+        //III. Function returns true if level changes
+             
+    }
+    */
 }
+
+
